acbf9d51ce36926e8229fa45abcf72d9678bf582
muhammadqasimashraf <muhammad.qasim@atomicity-interactive.com>
2023-07-01 05:12:36 -0700

    responsive cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,7 +4,7 @@ const Card = ({ item }) => {
   console.log(item);
 
   return (
-    <div className="col-md-3 text-white cardWidth mb-5">
+    <div className=" col-lg-3 col-md-6 col-sm-6 text-white cardWidth mb-5">
       <div className="data p-3 mb-4 bg-dark h-100 rounded">
         <div className="img mt-2 mb-2 text-center">
           <img
@@ -15,7 +15,7 @@ const Card = ({ item }) => {
           />
         </div>
         <div className="heading mt-3 mb-3  text-center">{item.name}</div>
-        <div className="comment mt-5  mb-0">{item.comment}</div>
+        <div className="comment mt-4   mb-0">{item.comment}</div>
       </div>
     </div>
   );
